6bc01456c31974f63f39cbc65bbe1629599e5979
Remo Laubacher <remo.laubacher@gmail.com>
2015-06-17 17:31:54 +0200

    check if file exists before renaming it, we don't want to overwrite existing files

--- tools/files/save_file_name.php
+++ tools/files/save_file_name.php
@@ -5,11 +5,27 @@ $fileName = Loader::helper('text')->sanitize($_REQUEST['fileName']);
 
 $file = File::getByID($fileId);
 
+// check if we already have a file with the target name
+$f = Loader::helper('concrete/file');
+if ($file->getStorageLocationID() > 0) {
+    $fsl = FileStorageLocation::getByID($file->getStorageLocationID());
+    $path = $f->mapSystemPath($file->getPrefix(), $fileName, false, $fsl->getDirectory());
+}
+else {
+    $path = $f->mapSystemPath($file->getPrefix(), $fileName, false);
+}
+if (file_exists($path)) {
+    echo t('File with the name "%s" already exists in the directory "%s"!', $fileName, $file->getPrefix());
+    die();
+}
+
+// get old paths of file
 $oldPath = $file->getPath();
 $oldPathThumbnail1 = $file->hasThumbnail(1) ? $file->getThumbnailPath(1) : false;
 $oldPathThumbnail2 = $file->hasThumbnail(2) ? $file->getThumbnailPath(2) : false;
 $oldPathThumbnail3 = $file->hasThumbnail(3) ? $file->getThumbnailPath(3) : false;
 
+// rename file in database
 $file->updateFile($fileName, $file->getPrefix());
 
 // rename files on disk
